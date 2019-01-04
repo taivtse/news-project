1f2c751eb1f785e5339284900f00e08e03410bd0
Võ Thành Tài <taivtse@gmail.com>
2019-01-04 21:56:59 +0700

    Complete the first models

---
+++ src/main/java/com/laptrinhjavaweb/model/CommentModel.java
@@ -0,0 +1,28 @@
+package com.laptrinhjavaweb.model;
+
+public class CommentModel extends AbstractModel{
+	private String content;
+	private int userId;
+	private int newsId;
+	
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public int getNewsId() {
+		return newsId;
+	}
+	public void setNewsId(int newsId) {
+		this.newsId = newsId;
+	}
+	
+	
+}
